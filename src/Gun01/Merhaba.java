aa2c51a75fef32dafbb1ec15fee3510bfda4f768
NabbyNoble <nebicanga@gmail.com>
2022-10-11 09:43:25 +0300

    Üçüncü taskımı bitirdim.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,9 +3,14 @@ package Gun01;
 public class Merhaba {
 
     public static void main(String[] args) {
-        System.out.println("Merhaba");
+
         for (int i = 0; i <5 ; i++) {
             System.out.println("Merhaba"+i);
         }
+        int sayac=0;
+        do {
+            System.out.println("Yeni Merhaba");
+            sayac++;
+        }while (sayac<5);
     }
 }
