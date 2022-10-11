747977c587d8b71a8f9835bde73922e503dceec5
NabbyNoble <nebicanga@gmail.com>
2022-10-11 09:19:35 +0300

    İkinci taskımı bitirdim.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,6 +3,9 @@ package Gun01;
 public class Merhaba {
 
     public static void main(String[] args) {
-
+        System.out.println("Merhaba");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Merhaba"+i);
+        }
     }
 }
