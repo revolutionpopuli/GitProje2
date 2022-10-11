b311e5c4e7a36e1e5cd17a43a901d6ff406e26d5
NabbyNoble <nebicanga@gmail.com>
2022-10-11 09:52:05 +0300

    Mesajda düzeltme yapıldı.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,7 +3,7 @@ package Gun01;
 public class Merhaba {
 
     public static void main(String[] args) {
-        System.out.println("Merhaba");
+        System.out.println("Merhaba Dünya");
         for (int i = 0; i <5 ; i++) {
             System.out.println("Merhaba"+i);
         }
